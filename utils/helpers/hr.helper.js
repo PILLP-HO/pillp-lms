f297984ed914d3d5a30570fbd103fc0549d17cf1
RoH0HaN <rohan251@outlook.com>
2025-07-31 14:09:37 +0530

    fix: hr helper method fixed

--- utils/helpers/hr.helper.js
+++ utils/helpers/hr.helper.js
@@ -4,7 +4,7 @@ import { formatWhatsappNumber } from "../utilities.js";
 const getHrData = () => {
   const hr = hrList.find(
     (hr) =>
-      hr["Designation"]?.toLowerCase() === "hr executive" &&
+      hr["Department"]?.toLowerCase() === "all" &&
       hr["Work Location"]?.toLowerCase() === "all"
   );
 
